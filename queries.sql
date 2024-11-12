a5c8c84c59454853ee002eb227419c3be87d0bf4
Jenifa <jendotopshatta@outlook.com>
2024-11-11 22:57:41 -0500

    Update queries.sql
    tasks from the assignments added

--- queries.sql
+++ queries.sql
@@ -1,1 +1,46 @@
+-- Assignments for a Specific Course
+SELECT title, due_date
+FROM assignments
+WHERE course_id LIKE 'COMP1234';
+
+-- Earliest Assignment Due Date
+SELECT min(due_date)
+FROM assignments;
+
+-- Latest Assignment Due Date
+SELECT max(due_date)
+FROM assignments;
+
+-- Assignments Due on a Specific Date
+SELECT title, course_id
+FROM assignments
+WHERE due_date = '2024-10-08';
+
+
+-- All Assignments Due in October
+SELECT title, due_date
+FROM assignments
+WHERE due_date like '2024-10%';
+
+-- Most Recent Completed Assignment
+SELECT max(due_date)
+FROM assignments
+WHERE status = 'Completed';
+
+-- Optional Tasks
+-- Count of "Not Started" Assignments
+SELECT count(*) 
+FROM assignments
+WHERE status != 'Started';
+
+-- Courses With Labs on Tuesday
+SELECT course_id, course_name
+FROM courses
+WHERE lab_time LIKE 'Tue%';
+
+
+
+
+
+
 
